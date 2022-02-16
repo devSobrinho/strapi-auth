b1b1a8bd163705c043b3f468f18b208579f10bc2
Daniel Sobrinho <dev.sobrinho@gmail.com>
2022-02-15 21:15:58 -0300

    Created global policies

---
+++ config/policies/is-logged-in.js
@@ -0,0 +1,9 @@
+module.exports = async (ctx, next) => {
+  console.log('GLOBAL POLICY');
+  if (ctx.state.user) {
+    // Go to next policy or will reach the controller's action.
+    return await next();
+  }
+
+  ctx.unauthorized(`You're not logged in!`);
+};
