8531df1b54b0a48eebfb8233a622ee2703454956
Daniel Sobrinho <dev.sobrinho@gmail.com>
2022-02-16 14:33:39 -0300

    Added provider smtp and create config file

--- config/plugins.js
+++ config/plugins.js
@@ -11,4 +11,25 @@ module.exports = ({ env }) => ({
       delete: {},
     },
   },
+
+  email: {
+    provider: 'smtp',
+    providerOptions: {
+      host: 'smtp.gmail.com', //SMTP Host
+      port: 465   , //SMTP Port
+      secure: true,
+      username: process.env.SMTP_EMAIL,
+      password: process.env.SMTP_PASSWORD,
+      rejectUnauthorized: true,
+      requireTLS: true,
+      connectionTimeout: 1,
+    },
+    settings: {
+      from: process.env.SMTP_EMAIL,
+      replyTo: process.env.SMTP_EMAIL,
+    },
+  },
 });
+
+
+
